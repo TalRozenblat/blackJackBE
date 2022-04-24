2fcb80040d4a8a7bfbd161517ca4f91a32c11058
Tal <rozenblattal@gmail.com>
2022-04-25 00:26:00 +0300

    added table creation incase they don't exist

--- connection.js
+++ connection.js
@@ -16,4 +16,29 @@ connection.connect((err) => {
     }
 });
 
+const usersTableQuery = "CREATE TABLE if not exists 'users' ('id' int NOT NULL AUTO_INCREMENT, 'email' varchar(100) NOT NULL, 'firstname' varchar(45) NOT NULL, 'lastname' varchar(45) NOT NULL, 'password' varchar(150) NOT NULL, 'nickname' varchar(45) NOT NULL, PRIMARY KEY ('id'), UNIQUE KEY 'email_UNIQUE' ('email'), UNIQUE KEY 'nickname_UNIQUE' ('nickname')) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+
+const scoresTableQuery = `CREATE TABLE if not exists scores (email varchar(100) NOT NULL, nickname varchar(45) NOT NULL, score int NOT NULL, date datetime NOT NULL) 
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci`;
+
+
+connection.query(usersTableQuery, (err, result) => {
+    if (err){
+     
+        return;
+    }
+    else{
+        return;
+    }
+});
+
+connection.query(scoresTableQuery, (err, result) => {
+    if (err){
+     
+        return;
+    }
+    else{
+        return;
+    }
+});
 export default connection;
